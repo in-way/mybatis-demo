47860f6aa36a5612d8e6f409c6e28b9f3a093774
weixiaolong <784615820@qq.com>
2021-09-06 09:00:08 +0800

    自定义返回状态码

--- src/main/java/com/batis/demo/controller/SwaggerTest.java
+++ src/main/java/com/batis/demo/controller/SwaggerTest.java
@@ -2,10 +2,7 @@ package com.batis.demo.controller;
 
 import com.batis.demo.annoation.GbpsRest;
 import com.gbps.webex.entity.GbpsResponse;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -26,6 +23,9 @@ public class SwaggerTest {
             @ApiImplicitParam(name="mobile",value="手机号",required=true,paramType="form"),
             @ApiImplicitParam(name="password",value="密码",required=true,paramType="form"),
             @ApiImplicitParam(name="age",value="年龄",required=true,paramType="form",dataType="Integer")
+    })
+    @ApiResponses({
+            @ApiResponse(code=200,message="请求成功"),
     })
     @GetMapping("/list")
     @GbpsRest
