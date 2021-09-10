94e21c5483b2756b0d2063474265962fccdf054d
weixiaolong <784615820@qq.com>
2021-09-10 11:39:02 +0800

    新增getBean

--- src/main/java/com/batis/demo/BatisApplication.java
+++ src/main/java/com/batis/demo/BatisApplication.java
@@ -1,13 +1,32 @@
 package com.batis.demo;
+import com.google.common.collect.Lists;
+import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class BatisApplication {
-
+    public static ApplicationContext applicationContext;
     public static void main(String[] args) {
         SpringApplication.run(BatisApplication.class, args);
     }
 
+    public static <T> T getBean(String name) {
+        try {
+            return (T) applicationContext.getBean(name);
+        } catch (BeansException e) {
+            return null;
+        }
+    }
+    public static <T> T getBean(Class<T> clazz) {
+        Map<String, T> beans = applicationContext.getBeansOfType(clazz);
+        if (beans.size() <= 0) {
+            return null;
+        }
+        return Lists.newArrayList(beans.values()).get(0);
+    }
 }
